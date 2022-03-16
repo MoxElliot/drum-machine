f9a3c7f8e4bb90221fcae0f1d81c5ca708899c78
Elliot Erickson <eric0952@gmail.com>
2022-03-16 09:26:49 -0700

    mapped drumpad buttons

--- src/elements/drum-display.js
+++ src/elements/drum-display.js
@@ -1,12 +1,16 @@
 import React from "react";
 import DrumPad from "../components/drum-pad";
 
+
 class DrumDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
     render () {
+      
       return (
         <div id="display" className='pad-container' style={{backgroundColor: "lightblue"}}>
-            drum display
-            <DrumPad />
+          <DrumPad />
         </div>
       )
     }
