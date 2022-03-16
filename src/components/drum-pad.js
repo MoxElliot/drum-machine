ac01aa04ca119b3d179d9cb22f6a3f357c67b5dd
Elliot Erickson <eric0952@gmail.com>
2022-03-16 15:37:57 -0700

    key press works

--- src/components/drum-pad.js
+++ src/components/drum-pad.js
@@ -1,21 +1,33 @@
 import React from "react";
 
+const DrumPad = ({ sound }) => {
 
+  const handleKeyDown = (e) => {
+    if (e.keyCode === sound.keyCode) {
+      playSound();
+    };
+  };
 
-
-const DrumPad = ({sound}) => {
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+      return () => {
+        window.removeEventListener('keydown',handleKeyDown)
+      }
+    })
 
   const playSound = () => {
     const playAudio = document.getElementById(sound.keyTrigger);
     playAudio.currentTime = 0;
     playAudio.play();
   }
-      return (
-        <div onClick={playSound}>
-          <audio id={sound.keyTrigger} src={sound.url}/>
-         {sound.keyTrigger} 
-         </div>
-         )
+
+
+  return (
+    <div onClick={playSound} className="drum-pad" tabIndex={sound.keyTrigger} onKeyDown={playSound}>
+      <audio id={sound.keyTrigger} src={sound.url}/>
+      {sound.keyTrigger} 
+      </div>
+      )
 
 }
 
