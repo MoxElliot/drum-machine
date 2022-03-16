d58ba040752fbebd1c6a3e2edb9d37a4a95ed4ff
Elliot Erickson <eric0952@gmail.com>
2022-03-16 14:19:35 -0700

    sounds play

--- src/components/drum-pad.js
+++ src/components/drum-pad.js
@@ -1,85 +1,22 @@
 import React from "react";
 
 
-class DrumPad extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-     name: "React",
-     bankOne: [
-    {
-      keyCode: 81,
-      keyTrigger: 'Q',
-      id: 'Heater-1',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-    },
-    {
-      keyCode: 87,
-      keyTrigger: 'W',
-      id: 'Heater-2',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-    },
-    {
-      keyCode: 69,
-      keyTrigger: 'E',
-      id: 'Heater-3',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-    },
-    {
-      keyCode: 65,
-      keyTrigger: 'A',
-      id: 'Heater-4',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-    },
-    {
-      keyCode: 83,
-      keyTrigger: 'S',
-      id: 'Clap',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-    },
-    {
-      keyCode: 68,
-      keyTrigger: 'D',
-      id: 'Open-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-    },
-    {
-      keyCode: 90,
-      keyTrigger: 'Z',
-      id: "Kick-n'-Hat",
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-    },
-    {
-      keyCode: 88,
-      keyTrigger: 'X',
-      id: 'Kick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-    },
-    {
-      keyCode: 67,
-      keyTrigger: 'C',
-      id: 'Closed-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-    }
-    ],
-    isPlaying: false
-    };
+
+
+const DrumPad = ({sound}) => {
+
+  const playSound = () => {
+    const playAudio = document.getElementById(sound.keyTrigger);
+    playAudio.currentTime = 0;
+    playAudio.play();
   }
+      return (
+        <div onClick={playSound}>
+          <audio id={sound.keyTrigger} src={sound.url}/>
+         {sound.keyTrigger} 
+         </div>
+         )
 
-    
-      render () {
-        
-
-          return (
-            this.state.bankOne.map(function(bank){
-          
-              return <button onClick={bank.url}> {bank.keyTrigger} </button>;
-            })
-      )}
-    
-  
-  
- };
-
+}
 
   export default DrumPad;
