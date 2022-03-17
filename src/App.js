8e7ba687ebe8bb7992bc1995843234487b2874e4
Elliot Erickson <eric0952@gmail.com>
2022-03-17 10:17:45 -0700

    moved all components to one file APP

--- src/App.js
+++ src/App.js
@@ -1,21 +1,121 @@
 import React from 'react';
-import Header from './elements/header.js';
-import DrumDisplay from './elements/drum-display.js';
-import VolumeBar from './elements/volume-bar.js';
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+]
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-  }
+ 
   render () {
     return (
       <div id="drum-machine" className='app-container'>
-        <Header />
+        <div className="header" style={{backgroundColor: "green"}}>
+            header
+        </div>
         <DrumDisplay />
-        <VolumeBar />
       </div>
     );
   }
+}
+
+const DrumDisplay = () => {
+ 
+  return (
+    <div className='control-container'>
+        <div id="display" className='pad-container'>
+          {bankOne.map ((sound) => (
+          <DrumPad  sound={sound} className="drum-pad"/>
+          )
+          )}
+        </div>
+      
+      <div className="display-bar">
+        <p className="display" >
+          Place Holder for Sound Display
+        </p>
+      </div>
+    </div>)
+}
+
+const DrumPad = ({ sound }) => {
+  console.log(sound);
+  const handleKeyDown = (e) => {
+    if (e.keyCode === sound.keyCode) {
+      playSound();
+    };
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+      return () => {
+        window.removeEventListener('keydown',handleKeyDown)
+      }
+    })
+
+  const playSound = () => {
+    const playAudio = document.getElementById(sound.keyTrigger);
+    
+    playAudio.currentTime = 0;
+    playAudio.play();
+  }
+
+  return (
+    <div onClick={playSound} id={sound.keyCode} class="drum-pad">
+      <audio id={sound.keyTrigger} className="clip" src={sound.url}/>
+      {sound.keyTrigger}   
+    </div>)
 }
 
 export default App;
--- src/elements/header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <div className="header" style={{backgroundColor: "green"}}>
-            header
-        </div>
-    )
-}
-
-export default Header;
